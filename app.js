29687b0afb9e0f1facd64b8f080c634d136c1a84
Aman Gupta <guptakunalx@gmail.com>
2023-05-26 17:58:49 +0530

    Prototype-Model

--- app.js
+++ app.js
@@ -2,11 +2,18 @@
 function User (username, email) {
     this.username = username;
     this.email = email;
-
-    this.login = function () {
-        console.log(`${this.username} has logged in!`);
-    }
 }
+
+// prototype model
+User.prototype.login = function () {
+    console.log(`${this.username} has logged in!`);
+    return this;
+};
+
+User.prototype.logout = function () {
+    console.log(`${this.username} has logged out!`);
+    return this;
+};
 
 // instance of the class
 const userOne = new User("mario", "mario@themarioclub.co.uk");
@@ -14,6 +21,8 @@ const userTwo = new User("luigi", "luigi@themarioclub.co.uk");
 
 console.log(userOne);
 userOne.login();
+
+userTwo.login().logout();
 
 // the `new keyword`
 // 1 - it creates a new empty object `{}`
