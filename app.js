1c125e4f6a14ef95764cda4ec544001b7801ef76
Aman Gupta <guptakunalx@gmail.com>
2023-05-26 17:16:16 +0530

    Class-Inheritance

--- app.js
+++ app.js
@@ -25,21 +25,43 @@ class User {
 
 }
 
+class Admin extends User {
+
+    deleteUser(user) {
+
+        users = users.filter(u => u.username !== user.username);
+
+    }
+
+}
+
 // instance of the class
 const userOne = new User("mario", "mario@themarioclub.co.uk");
 const userTwo = new User("luigi", "luigi@themarioclub.co.uk");
+const userThree = new Admin("ryu", "ryu@themarioclub.co.uk");
 
 console.log(userOne, userTwo);
+
+let users = [userOne, userTwo, userThree];
+
+userThree.deleteUser(userTwo);
+console.log(users);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // the `new keyword`
 // 1 - it creates a new empty object `{}`
 // 2 - it bind the value of `this` to the newly created empty object
 // 3 - it calls the constructor function to `build` the object
 
-console.log(`USER ONE`);
-userOne.login();
-userOne.logout();
-
-// Method Chaining
-console.log(`USER TWO`);
-userTwo.login().inScore().inScore().inScore().logout();
