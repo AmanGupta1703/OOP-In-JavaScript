5446f3daf5b3c6c3c50b794ce9e40d72bc557f5f
Aman Gupta <guptakunalx@gmail.com>
2023-05-26 18:16:50 +0530

    Prototypal-Inheritance

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
 // CONSTRUCTOR FUNCTION
-function User (username, email) {
-    this.username = username;
-    this.email = email;
+function User(username, email) {
+	this.username = username;
+	this.email = email;
 }
 
 // prototype model
 User.prototype.login = function () {
-    console.log(`${this.username} has logged in!`);
-    return this;
+	console.log(`${this.username} has logged in!`);
+	return this;
 };
 
 User.prototype.logout = function () {
-    console.log(`${this.username} has logged out!`);
-    return this;
+	console.log(`${this.username} has logged out!`);
+	return this;
 };
+
+// Admin class
+function Admin(username, email, title) {
+	User.call(this, username, email);
+    this.title = title; 
+}
+
+Admin.prototype = Object.create(User.prototype); // creates a new prototype and bases it to the User prototype
+
+Admin.prototype.deleteUsers = function () {
+    // delete a user
+}
 
 // instance of the class
 const userOne = new User("mario", "mario@themarioclub.co.uk");
 const userTwo = new User("luigi", "luigi@themarioclub.co.uk");
+const userThree = new Admin("ryu", "ryu@themarioclub.co.uk", "ninja-black-belt");
 
-console.log(userOne);
+console.log(userOne, userTwo, userThree);
 userOne.login();
 
 userTwo.login().logout();
